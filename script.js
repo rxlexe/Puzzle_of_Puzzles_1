8f95ae0875558f7c2a9275de80604ab752c89253
Lexy Ehlert <138158156+rxlexe@users.noreply.github.com>
2023-08-30 20:06:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2708
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+document.addEventListener("DOMContentLoaded", function() {
+  const nouns = ["Trinket", "Bauble", "Doodle", "Gadget", "Tidbit", "Knickknack", "Whimsy", 
+ "Fluff", "Glimmer", "Hubbub", "Morsel", "Flicker", "Jumble", "Snippet", 
+ "Fringe", "Puddle", "Quirk", "Zest", "Wisp", "Sprinkle", "Fumble", "Scurry", 
+ "Tangle", "Glimpse", "Riffle", "Clutter", "Tweak", "Nibble", "Smidgen", 
+ "Snuggle", "Dabble", "Crumble", "Drizzle", "Glint", "Skirmish", "Flutter", 
+ "Smudge", "Tumble", "Squiggle", "Scuffle"];
+  
+  const ciphers = [
+    "A=P", "B=Q", "C=R", "D=S", "E=T",
+    "F=G", "G=V", "H=W", "I=X", "J=Y",
+    "K=Z", "L=A", "M=B", "N=C", "O=D",
+    "P=E", "Q=F", "R=G", "S=H", "T=I",
+    "U=J", "V=K", "W=L", "X=M", "Y=N",
+    "Z=O", "A=C", "B=D", "C=E", "D=F",
+    "E=G", "F=H", "G=I", "H=J", "I=K",
+    "J=L", "K=M", "L=N", "M=O", "N=P"
+  ];
+  
+  const grid = document.querySelector('.grid');
 
-\f0\fs24 \cf0 document.addEventListener("DOMContentLoaded", function() \{\
-    const validCombinations = \{\
-        "username1": "password1",\
-        "username2": "password2",\
-        "username3": "password3",\
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-\cf0         "username4\'94: "password4\'94,\
-        "username5\'94: "password5\'94,\
-        "username6\'94: "password6\'94,\
-        "username7\'94: "password7,\
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-\cf0         "username8": "password8"\
-    \};\
-\
-    const form = document.getElementById("authForm");\
-    form.addEventListener("submit", function(event) \{\
-        event.preventDefault();\
-        const username = document.getElementById("username").value;\
-        const password = document.getElementById("password").value;\
-\
-        if (validCombinations[username] === password) \{\
-            document.getElementById("result").textContent = "Congratulations! You have passed the first test. Your next clue will be mailed to you in the next two weeks, but you will need this cipher to solve it: A=P";\
-        \} else \{\
-            document.getElementById("result").textContent = "Invalid username or password.";\
-        \}\
-    \});\
-\});\
-}
+  for (let i = 0; i < 40; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    
+    const card = document.createElement('div');
+    card.classList.add('card');
+    
+    const cardFront = document.createElement('div');
+    cardFront.classList.add('card-front');
+    cardFront.textContent = nouns[i];
+    
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('card-back');
+    cardBack.textContent = ciphers[i];
+    
+    card.appendChild(cardFront);
+    card.appendChild(cardBack);
+    box.appendChild(card);
+    
+    box.addEventListener('click', function() {
+      card.classList.toggle('card-flipped');
+    });
+    
+    grid.appendChild(box);
+  }
+});
